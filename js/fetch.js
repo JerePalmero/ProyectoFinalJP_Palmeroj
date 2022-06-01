d3e1420fd2491310d47c84631ee5aeadae6a286a
Jeremias Palmero <86244225+JerePalmero@users.noreply.github.com>
2022-06-01 17:25:31 -0300

    Correccion fitch

--- js/fetch.js
+++ js/fetch.js
@@ -1,7 +1,6 @@
-/*En mi proyecto no voy a utilizar fetch pero bueno aca va uno que te trae todos los nombres 
-de los Pokmon a un consol log ...inspeccionar la pagina en index y verificar en consola.  */
 
-fetch('https://pokeapi.co/api/v2/pokemon')
+
+fetch('js/data.json')
   .then(response => response.json()) 
   .then(data => console.log(data))
 
